ca59a32183a4a5e6a790da118d9d82070d71ee1a
Siraj <Siraj@MacBook-Pro-de-MacbookSSD.local>
2018-11-01 23:23:20 +0100

    afficher details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
+
+
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  city$: Object;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private data: DataService) 
+  {
+    this.route.params.subscribe( params => this.city$ = params.id );
+
+  }
+
+  ngOnInit() 
+  {
+    this.data.getCity(this.city$).subscribe(
+      data => this.city$ = data 
+    );
   }
 
 }
